5c5b346882a7db56d2d56b7215ba3da02cd28961
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2022-05-07 11:12:11 -0300

    ElPepe

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -4,6 +4,8 @@ import axios from "axios";
 const authApi = axios.create({
 
     baseURL: import.meta.env.VITE_API_URL
+
+    // el pepe
  
     // headers: { 
     //     'Content-Type': 'application/json',
